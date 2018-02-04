74bdd7102dd293c2b2b26529ace8a64d4c8cf098
Maria Vershinina <mariavershina@gmail.com>
2018-02-04 17:24:51 +0300

    создание события: добавление в db и отображение в сетке

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -1,5 +1,10 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+import { graphql, compose } from 'react-apollo';
+import moment from 'moment';
+
+import { CREATE_EVENT_QUERY, GET_EVENTS_QUERY } from '../queries';
 
 class CreateEvent extends Component {
     constructor(props) {
@@ -17,19 +22,47 @@ class CreateEvent extends Component {
         })
     }
 
+    _onClick = () => {
+        const {title, date, startTime, endTime, users, room} = this.props;
+        let dateStart = moment(date, 'DD.MM.YYYY').set({"hours": startTime.slice(0, 2), "minutes": startTime.slice(3, 5)}).format();
+        let dateEnd = moment(date, 'DD.MM.YYYY').set({"hours": endTime.slice(0, 2), "minutes": endTime.slice(3, 5)}).format();
+
+        this.props.createEvent({
+            variables: {
+                eventInput: {
+                    dateStart: dateStart,
+                    dateEnd: dateEnd,
+                    title: title
+                },
+                usersIds: users.map(user => user.id),
+                roomId: room.id
+            },
+            refetchQueries: [{
+            query:GET_EVENTS_QUERY
+            }]
+        })
+        .then(({ data: {createEvent} }) => {
+            this.props.history.push({
+                pathname: '/',
+                state: createEvent
+            })
+        })
+    };
 
     render() {
         let isDisabled = this.state.isDisabled;
         const classDisabled = (isDisabled) ? " disabled" : " ";
 
         return (
-            <Link to="/#popup">
-                <button className={"button" + classDisabled}  disabled={isDisabled}>
+            <Link to="/">
+                <button className={"button" + classDisabled}  disabled={isDisabled} onClick={this._onClick}>
                     Создать встречу
                 </button>
             </Link>
         )
-    }
+    };
 }
 
-export default CreateEvent;
+export default compose(
+    graphql(CREATE_EVENT_QUERY, {name: 'createEvent'})
+)(withRouter(CreateEvent))
--- src/queries/index.js
+++ src/queries/index.js
@@ -8,8 +8,7 @@ query UsersQuery {
         homeFloor
         avatarUrl
     }
-}
-`;
+}`;
 
 export const ROOMS_QUERY = gql`
 query RoomsQuery {
@@ -19,5 +18,35 @@ query RoomsQuery {
         capacity
         floor
     }
-}
-`;
+}`;
+
+export const CREATE_EVENT_QUERY = gql`
+mutation CreateEvent($eventInput: EventInput!, $usersIds: [ID], $roomId: ID!, ) {
+    createEvent(input: $eventInput, usersIds: $usersIds, roomId: $roomId) {
+        title
+        dateStart
+        dateEnd
+        room {
+            title
+            floor
+        }
+    }
+}`;
+
+export const GET_EVENTS_QUERY = gql`
+query GetEvents {
+    events {
+        id
+        dateStart
+        dateEnd
+        title
+        room {
+            id
+        }
+    users {
+        homeFloor
+        login
+        avatarUrl
+      }
+    }
+}`;
